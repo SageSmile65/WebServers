b54af69777311e6a6737876ec1bf036cbaef602c
Akshat <pathakakshat51@outlook.com>
2026-06-22 06:56:59 +0000

    create client class

---
+++ src/Client.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    int port = 8080;
+    public void run() throws IOException {
+        InetAddress address = InetAddress.getLocalHost();
+        Socket socket = new Socket(address,port );
+
+        PrintWriter toServer = new PrintWriter(socket.getOutputStream());
+        BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        toServer.println("Hello from client");
+
+        String response = fromServer.readLine();
+        System.out.println(response);
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Client().run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
